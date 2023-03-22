0d9ef81f29a96fb5f2107cef43dffe387ecfc1e3
Remi Labbe <labbe.remi@free.fr>
2023-03-22 14:54:05 +0100

    move backend config to vars file for each env

--- main.tf
+++ main.tf
@@ -6,10 +6,6 @@ terraform {
     }
   }
   backend "azurerm" {
-    resource_group_name  = "rg-tfstate"
-    storage_account_name = "samfolabs2023"
-    container_name       = "tfstate"
-    key                  = "rlabbe.tfstate"
 
   }
 }
