e9567b97624c261a4a8358be348c5b7c3b8b638c
Remi Labbe <labbe.remi@free.fr>
2023-03-22 10:30:45 +0100

    create first resource group

--- main.tf
+++ main.tf
@@ -1,12 +1,18 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "3.48.0"
     }
   }
 }
 
 provider "azurerm" {
-  # Configuration options
+  features {}
+}
+
+# type d'objet - "nom resource" - "identifiant resource"
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-rlabbe"
+  location = "West Europe"
 }
