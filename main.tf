ca5ba5e9d9d2a744978efbc1a333aae96178153b
Remi Labbe <labbe.remi@free.fr>
2023-03-23 10:28:35 +0100

    add rabbitmq container group

--- main.tf
+++ main.tf
@@ -61,8 +61,39 @@ resource "azurerm_linux_web_app" "lwa" {
   }
 
   app_settings = {
-    RabbitMQ__Hostname = "rabbitmq"
+    RabbitMQ__Hostname = azurerm_container_group.rabbitmq.fqdn
     RabbitMQ__Username = data.azurerm_key_vault_secret.rabbitmq-login.value
     RabbitMQ__Password = data.azurerm_key_vault_secret.rabbitmq-password.value
+  }
+}
+
+resource "azurerm_container_group" "rabbitmq" {
+  name                = "aci-mq-${var.project_name}${var.environment_suffix}"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  ip_address_type     = "Public"
+  dns_name_label      = "aci-mq-${var.project_name}${var.environment_suffix}"
+  os_type             = "Linux"
+
+  container {
+    name   = "rabbitmq"
+    image  = "rabbitmq:3-management"
+    cpu    = "0.5"
+    memory = "1.5"
+
+    ports {
+      port     = 5672
+      protocol = "TCP"
+    }
+
+    ports {
+      port     = 15672
+      protocol = "TCP"
+    }
+
+    environment_variables = {
+      RABBITMQ_DEFAULT_USER = data.azurerm_key_vault_secret.rabbitmq-login.value
+      RABBITMQ_DEFAULT_PASS = data.azurerm_key_vault_secret.rabbitmq-password.value
+    }
   }
 }
